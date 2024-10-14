a628f0a879e6b38e9d4f3ee7c0e78f6f2292b4c7
DarlanPrado <darlandoprado2014@gmail.com>
2024-10-14 16:47:54 -0300

    fix(useHttp): removed unnecessary use of useState

--- src/runtime/composables/useHttp.ts
+++ src/runtime/composables/useHttp.ts
@@ -1,47 +1,45 @@
 import type { FetchOptions } from 'ofetch'
-import { createError, useAppConfig, useState } from '#imports'
+import { createError, useAppConfig } from '#imports'
 
-declare module 'nuxt/schema' {
-  interface AppConfigInput {
-    http?: {
-      endpoints: HttpEndpoints[]
-    }
-  }
-}
-
-export interface HttpEndpoints {
+interface HttpEndpoints {
   name: string
   fetchOptions: FetchOptions
 }
 
-export interface UseHttpReturn {
+interface HttpSchema {
+  endpoints: HttpEndpoints[]
+}
+
+declare module 'nuxt/schema' {
+  interface AppConfigInput {
+    http?: HttpSchema
+  }
+}
+
+interface UseHttpReturn {
   add: (name: string, fetchOptions: FetchOptions) => FetchOptions
   endpoints: string[]
   request: (endpointName: string) => { fetch: ReturnType<typeof $fetch.create> }
 }
 
 export function useHttp(): UseHttpReturn {
-  const httpEndpoints = useState<HttpEndpoints[]>('http-endpoints', () => {
-    // @ts-expect-error: AppConfig type is a relationship of a key being a string with an unknown value, which causes an error when trying to use http.endpoints
-    return useAppConfig().http?.endpoints || []
-  })
-
-  const endpoints = httpEndpoints.value.map(endpoint => endpoint.name)
+  const httpEndpoints = (useAppConfig().http as HttpSchema || null)?.endpoints || []
+  const endpointsNames = httpEndpoints.map(endpoint => endpoint.name)
 
   function add(name: string, fetchOptions: FetchOptions) {
-    const index = httpEndpoints.value.findIndex(endpoint => endpoint.name === name)
+    const index = httpEndpoints.findIndex(endpoint => endpoint.name === name)
 
     if (index === -1) {
-      httpEndpoints.value.push({ name, fetchOptions } as HttpEndpoints)
+      httpEndpoints.push({ name, fetchOptions } as HttpEndpoints)
     }
     else {
-      httpEndpoints.value[index] = { name, fetchOptions } as HttpEndpoints
+      httpEndpoints[index] = { name, fetchOptions } as HttpEndpoints
     }
     return fetchOptions
   }
 
   function request(endpointName: HttpEndpoints['name']) {
-    const endpointOptions = httpEndpoints.value.find(endpoint => endpoint.name === endpointName)?.fetchOptions
+    const endpointOptions = httpEndpoints.find(endpoint => endpoint.name === endpointName)?.fetchOptions
 
     if (!endpointOptions) {
       throw createError(`Endpoint "${endpointName}" not exist.`)
@@ -54,7 +52,7 @@ export function useHttp(): UseHttpReturn {
 
   return {
     add,
-    endpoints,
+    endpoints: endpointsNames,
     request,
   }
 }
